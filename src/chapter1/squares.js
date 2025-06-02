ea881337e5c48506c4a9c9dec04c009e53c8c9f9
ALENUI <rovisalenui@gmail.com>
2025-06-02 23:54:45 +0100

    understanding typescript structure and type annotation

---
+++ src/chapter1/squares.js
@@ -0,0 +1,11 @@
+function squares(array) {
+    const result = array.map(x => x * x);
+    return result;
+}
+let variable;
+variable = 5;
+let var2 = 3;
+// var2="number"; // This will cause an error because var2 is inferred as a number
+function add(x, y) {
+    return x + y;
+}
---
+++ src/chapter1/squares.ts
@@ -0,0 +1,18 @@
+function squares (array: number[]){
+    const result = array.map(x=>x*x)
+    return result
+}
+
+let  variable:number;
+
+variable= 5;
+
+let var2=3;
+// var2="number"; // This will cause an error because var2 is inferred as a number
+
+function add (x:number,y:number){
+    return x+y
+}
+
+add(5,6); // This will work
+// add(1,2,3) // This will cause an error because the function expects exactly two arguments
